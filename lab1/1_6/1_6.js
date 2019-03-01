cd936338b9f9152c479ca5ae1ee078a67ce43705
Vlad <admin@MacBook-Pro-Vladislav.local>
2019-03-01 12:38:26 +0300

    задание 6
    Задание "Получить знак числа" выполнено

---
+++ lab1/1_6/1_6.js
@@ -0,0 +1,9 @@
+var request = prompt("Введите целое число: ", '');
+if (request > 0) {
+        alert ('1');
+    }
+else if (request < 0) {
+        alert ("-1");  
+    } else {
+        alert ("0");
+    }
