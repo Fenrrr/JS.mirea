5de6b8ea3034f028fd09bc22403ec9769522ef2e
Vlad <knyazev9797@mail.ru>
2019-04-10 18:49:07 +0300

    задание 3
    Задание "Сумма произвольного количества скобок" выполнено

---
+++ lab3/3/3_3/3_3.js
@@ -0,0 +1,16 @@
+function sum (a) {
+    var currentSum = a;
+
+    function f(b) {
+        currentSum += b;
+        return f;
+    }
+
+    f.toString = function () {
+        return currentSum;
+    };
+    return f;
+}
+
+alert(sum (1)(2)(3));
+alert(sum (-2)(-3)(-5));
